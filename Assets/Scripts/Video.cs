0b33dcf45becbd2171e2a8ddb6b53a83a396cde7
Tikhul <tikhul@mail.ru>
2019-10-23 22:01:46 +0400

    Play video when card is detected

---
+++ Assets/Scripts/Video.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Vuforia;
+
+
+public class Video : MonoBehaviour
+{
+    // Business card image target
+    public ImageTargetBehaviour image;
+    // Video inside the plain quad
+    public VideoPlayer videoPlayer;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If there's no image or image is not tracked, the video is paused
+        if(image == null || image.CurrentStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            videoPlayer.Pause();
+           // Debug.Log("Video paused");
+        }
+        else
+        {
+            videoPlayer.Play();
+           // Debug.Log("Video playing");
+        }
+        
+    }
+}
