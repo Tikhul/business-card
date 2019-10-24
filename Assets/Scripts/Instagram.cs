0122d412f67cd6895540f8f411ade66990ac553d
Tikhul <tikhul@mail.ru>
2019-10-24 13:19:32 +0400

    Add links to the website and Instagram buttons

---
+++ Assets/Scripts/Instagram.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instagram : MonoBehaviour
+{
+    // Open Instagram on click
+    public void InstagramOpen()
+    {
+        Application.OpenURL("instagram://user?blackvrar={blackvrar}");
+    }
+}
---
+++ Assets/Scripts/Website.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Website : MonoBehaviour
+{
+    // Open website on click
+    public void WebsiteOpen()
+    {
+        Application.OpenURL("https://blackvr.org/");
+    }
+}
